3ed0da1ffc65161a437938dbb8d249f6da8d6d9c
Reed MacConnell <Learn22Ruby@gmail.com>
2015-07-16 17:03:29 -0400

    got my grid to work, added none functioning buttons

--- js/sketchy.js
+++ js/sketchy.js
@@ -1,20 +1,26 @@
-var num = prompt("how many?", 100);
-
 $(document).ready(function() {
-  for(j=0;j < num; j++){
-    $('#container').append('<div />');
-    for(i=0;i < num; i++) {
-      $('#container').append('<div class="square" />');
-    };
-  };
-  $('.square').on('click', function() {
-    $(this).toggleClass('blacked');
-  });
-  $('.square').on('mouseenter',function() {
-    $(this).addClass('highlight');
+  var num = prompt("how many rows/columns? Between 1 and 64.", 64);
+  //prompt for buttons needs to go here. I want to offer three different
+  //ways to play with this
+  //Also I want the choices to use jQuery to appear
 
-  });
-  $('.square').on('mouseleave', function() {
-    $(this).removeClass('highlight');
-  });
+  if (num > 1 && num < 65) {
+    for(j=0;j < num; j++){
+      $('#container').append('<div />');
+      for(i=0;i < num; i++) {
+        $('#container').append('<div class="square" />');
+      };
+    };
+    $('.square').on('click', function() {
+      $(this).toggleClass('blacked');
+    });
+    $('.square').on('mouseenter',function() {
+      $(this).addClass('highlight');
+    });
+    $('.square').on('mouseleave', function() {
+      $(this).removeClass('highlight');
+    });
+  } else {
+    alert("Sorry that won't work.");
+  };
 });
