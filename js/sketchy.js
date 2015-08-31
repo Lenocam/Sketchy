6bcee5d0e2ac193e0fa5a60f560c96e78387bf24
Reed MacConnell <Learn22Ruby@gmail.com>
2015-08-31 15:54:08 -0400

    updated buttons got the first one to work so that it brings up the first choice to do sketching, need to invent the other versions now that I know I can get the buttons to work

--- js/sketchy.js
+++ js/sketchy.js
@@ -1,25 +1,54 @@
 $(document).ready(function() {
-  console.log("Ready!") //test if ready
-  var num = prompt("how many rows/columns? Between 1 and 64.", 64);
-  console.log(num);//test if num set
 
-  if (num > 1 && num < 65) {
-    for(j=0;j < num; j++){
-      $('#container').append('<div />');
-      for(i=0;i < num; i++) {
-        $('#container').append('<div class="square" />');
-      };
-    };
-    $('.square').on('click', function() {
-      $(this).toggleClass('blacked');
-    });
-    $('.square').on('mouseenter',function() {
-      $(this).addClass('highlight');
-    });
-    $('.square').on('mouseleave', function() {
-      $(this).removeClass('highlight');
-    });
-  } else {
-    alert("Sorry that won't work.");
+  function getNum() {
+    var numbers = prompt("how many rows/columns? Between 1 and 64.", 64);
+    return numbers;
   };
+  function createSketch() {
+    var num = getNum();
+    if (num > 1 && num < 65) {
+      for(j=0;j < num; j++){
+        $('#container').append('<div />');
+        for(i=0;i < num; i++) {
+          $('#container').append('<div class="square" />');
+        }
+      }
+    }
+  };
+
+  $('.btn').on('click', function() {
+    $(this).toggleClass("checked");
+    $(this).siblings('.checked').removeClass('checked');
+    var switchSketch = $(this).attr('id');
+
+    console.log(switchSketch);
+
+    switch (switchSketch) {
+      case 'btn1':
+          createSketch();
+
+          $('.square').on('click', function() {
+            $(this).toggleClass('blacked');
+          });
+          $('.square').on('mouseenter',function() {
+            $(this).addClass('highlight');
+          });
+          $('.square').on('mouseleave', function() {
+            $(this).removeClass('highlight');
+          });
+
+        console.log("howdy");
+        break;
+      case 'btn2':
+        console.log("hi");
+        break;
+
+      case 'btn3':
+        console.log("bye");
+        break;
+
+      default:
+        console.log("didnt work");
+    }
+  });
 });
